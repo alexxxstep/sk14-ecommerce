e5722f74d617d76ab12398fe6157065ac3a407df
alexxxstep <astep2004@gmail.com>
2020-10-31 12:17:07 +0200

    firs commit

---
+++ client/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <ul className="flex bg-indigo-800">
+        <li className="flex-1 mr-2 bg-indigo-800">Active Items</li>
+        <li className="flex-1 mr-2 bg-indigo-800">Nav Item</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
